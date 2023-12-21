190e3350132e88736b31b20fe591fa1c3af4bd06
Roma <romakvikvinia@gmail.com>
2023-12-21 18:21:01 +0400

    add valdiation

--- client/src/components/form/LoginForm.tsx
+++ client/src/components/form/LoginForm.tsx
@@ -1,4 +1,3 @@
-import React, { useCallback } from "react";
 import {
   Button,
   InputContainer,
@@ -8,25 +7,45 @@ import {
 // styles
 import styles from "./index.module.scss";
 import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface ILogin {
+  email: string;
+  password: string;
+}
 
 export const LoginForm = () => {
-  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  }, []);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILogin>();
+
+  const onSubmit: SubmitHandler<ILogin> = (data) => console.log(data);
+
+  console.log(errors);
+
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <InputContainer>
         <InputLabel htmlFor="email" aria-autocomplete="none">
           Email
         </InputLabel>
-        <InputField id="email" />
+        <InputField
+          id="email"
+          {...register("email", { required: "Email is required" })}
+        />
       </InputContainer>
 
       <InputContainer className={styles.loginFormPassword}>
         <InputLabel htmlFor="password" aria-autocomplete="none">
           Password
         </InputLabel>
-        <InputField type="password" id="password" />
+        <InputField
+          type="password"
+          id="password"
+          {...register("password", { required: "Password is required" })}
+        />
       </InputContainer>
       <Button type="submit">Login</Button>
       <div className={styles.footerText}>
--- client/src/components/form/RegisterForm.tsx
+++ client/src/components/form/RegisterForm.tsx
@@ -1,4 +1,3 @@
-import React, { useCallback } from "react";
 import {
   Button,
   InputContainer,
@@ -8,30 +7,58 @@ import {
 // styles
 import styles from "./index.module.scss";
 import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IRegister {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
 
 export const RegisterForm = () => {
-  const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  }, []);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IRegister>();
+
+  const onSubmit: SubmitHandler<IRegister> = (data) => console.log(data);
+
+  console.log(errors);
+
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
       <InputContainer>
         <InputLabel htmlFor="email">Email</InputLabel>
-        <InputField id="email" />
+        <InputField
+          id="email"
+          {...register("email", { required: "Email is required" })}
+        />
       </InputContainer>
       <section className={styles.nameFieldRow}>
         <InputContainer>
           <InputLabel htmlFor="firstName">First Name</InputLabel>
-          <InputField id="firstName" />
+          <InputField
+            id="firstName"
+            {...register("firstName", { required: "FirstName is required" })}
+          />
         </InputContainer>
         <InputContainer>
           <InputLabel htmlFor="lastName">Last Name</InputLabel>
-          <InputField id="lastName" />
+          <InputField
+            id="lastName"
+            {...register("lastName", { required: "LastName is required" })}
+          />
         </InputContainer>
       </section>
       <InputContainer>
         <InputLabel htmlFor="password">Password</InputLabel>
-        <InputField type="password" id="password" />
+        <InputField
+          type="password"
+          id="password"
+          {...register("password", { required: "Password is required" })}
+        />
       </InputContainer>
       <Button type="submit" className={styles.button}>
         Create My Account
